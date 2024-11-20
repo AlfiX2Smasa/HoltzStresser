a16689174242b84a4c1692d5d2abd75f31db18fa
AlfiX2Smasa <alfinorifannixco@gmail.com>
2024-11-20 12:46:36 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2024 HoltzStresser. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+}
